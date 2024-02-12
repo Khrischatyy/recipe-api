190f5eab3d0d7707676f2dd2f97daecd2d329a2a
khrischatyy <tony@machine-2.local>
2024-02-12 12:30:24 -0800

    dev/ made user api

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
     'rest_framework',
     'drf_spectacular',
-    'core',
+    'rest_framework.authtoken',
+    'user',
 ]
 
 MIDDLEWARE = [
